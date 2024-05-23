c7651e409e8cb584ddda64d16ad06ae7b8804b90
SandeshBhagat <sandeshb887@gmail.com>
2024-05-24 01:19:47 +0530

    header changes

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,10 +1,24 @@
+import "../css/header.css";
 
 const Header = () => {
   return (
-    <div>
-      this is header
-    </div>
-  )
-}
+    <header>
+      <h2>Furniish</h2>
+      <nav>
+        <ul>
+          <li>
+            <a href="">Find a Store</a>
+          </li>
+          <li>
+            <a href="">Services</a>
+          </li>
+          <li>
+            <a href="">Help</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
